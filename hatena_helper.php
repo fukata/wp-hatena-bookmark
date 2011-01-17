1e52203f3b4e42d67f0d225a787e1d6ebc894670
Tatsuya Fukata <tatsuya.fukata@gmail.com>
2011-01-17 19:29:31 +0800

    柔軟なパラメータ指定に対応

--- hatena_helper.php
+++ hatena_helper.php
@@ -1,9 +1,10 @@
 <?php
 
-function hatena_entries($url, $count=5) {
+function hatena_entries($blogurl, $filter=array('count'=>5,'sort'=>'count')) {
+	$url = _hatena_entries_url($blogurl, $filter);
 	$entries = _get_hatena_entries_cache($url);
 	if (empty($entries)) {
-		$url = "http://b.hatena.ne.jp/entrylist?sort=count&url=$url&mode=rss";
+		$url = "http://b.hatena.ne.jp/entrylist?url=$url&mode=rss";
 		$rdf = file_get_contents($url);	
 		$rdf = str_replace('dc:date', 'dc_date', $rdf);
 		$rdf = str_replace('dc:subject', 'dc_subject', $rdf);
@@ -19,6 +20,14 @@ function hatena_entries($url, $count=5) {
 		$list[] = $entries[$i];
 	}
 	return $list;
+}
+
+function _hatena_entries_url($blogurl, $filter) {
+	$url = "http://b.hatena.ne.jp/entrylist?url=$blogurl";
+	$url .= count($filter)>0 ? "&" : "";
+	$url .= implode("&", $filter);
+	$url .= "&mode=rss";
+	return $url;
 }
 
 function _hatena_popular_cache($url) {
